d785fe22c5288190d3cb073b8a4ef99f617d1498
Eduardo Helfer <eduardohelfer@gmx.net>
2023-07-13 19:53:25 -0500

    fix: menu structure

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -16,7 +16,6 @@ const Menu = () => {
     if(submenu){
       
       return (
-        /*<Nav.Link href={link}>{title}</Nav.Link>*/
         <NavDropdown title={title} id="basic-nav-dropdown">
           {
             submenu.map((item)=>(
@@ -26,8 +25,7 @@ const Menu = () => {
             ))
           }
         </NavDropdown>
-        
-      )
+     )
     }
     else {
       return(
@@ -44,17 +42,13 @@ const Menu = () => {
         <Navbar.Collapse id="navbarScroll"> 
           <Nav
             className="me-auto my-2 my-lg-0"
-            /*style={{ maxHeight: '100px' }}*/
             navbarScroll
           > 
-
-            {
-              menuData.menu.map((item)=>(
-                <MenuItem key={item.title} title={item.title} link={item.link} submenu={item.submenu} />
-              ))
-            }
-                
-
+          {
+            menuData.menu.map((item)=>(
+              <MenuItem key={item.title} title={item.title} link={item.link} submenu={item.submenu} />
+            ))
+          }
           </Nav>
           <Form className="d-flex">
             <Form.Control
@@ -73,67 +67,4 @@ const Menu = () => {
 
 export default Menu
 
-/*
-
-
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="basic-nav-dropdow">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-
-
-
-
-
----------------------------------------------------
-  const DropItem = ({title, link})=>{
-    console.log()
-    return (
-      <NavDropdown.Item id={title} href={link}>
-        {title}
-      </NavDropdown.Item>
-    )
-  }
-<DropItem key={item.title} title={item.title} link={item.link} /> 
-
-
-            {
-              menuData.menu.map((item)=>(
-                <MenuItem key={item.title} title={item.title} link={item.link} submenu={item.submenu} />
-              ))
-            }
-
-  const MenuItem = ({title, link, submenu}) => {
-    if(submenu){
-      
-      return (
-        <NavDropdown title={title} id="basic-nav-dropdown">
-          {
-            submenu.map((item)=>{
-              <NavDropdown.Item id={title} href={link}>
-                {title}
-              </NavDropdown.Item>
-            })
-          }
-        </NavDropdown>
-      )
-    }
-    else {
-      return(
-        <Nav.Link href={link}>{title}</Nav.Link>
-      )
-    }
-  }
-*/
 
